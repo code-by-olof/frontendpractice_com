5d00a2458ab84aa5279cc2e5c05100c7992ab193
code-by-olof <olofbaage@gmail.com>
2024-01-17 12:06:39 +0100

    Work in Progress

--- bonsai/public/js/app.js
+++ bonsai/public/js/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("test");
+const btnToggleNav = document.getElementById('toggle-nav');
+const navClassList = document.querySelector('.nav');
+const toggleMenu = () => {
+    if (navClassList.classList[1] == 'nav--desktop') {
+        navClassList.classList.remove('nav--desktop');
+        navClassList.classList.add('nav--mobile');
+    }
+    else {
+        navClassList.classList.remove('nav--mobile');
+        navClassList.classList.add('nav--desktop');
+    }
+};
+btnToggleNav.addEventListener('click', (event) => toggleMenu());
--- bonsai/src/js/app.ts
+++ bonsai/src/js/app.ts
@@ -1,1 +1,17 @@
-console.log("test");
+const btnToggleNav = document.getElementById('toggle-nav') as HTMLElement;
+const navClassList = document.querySelector('.nav') as HTMLElement;
+
+
+const toggleMenu = (): void => {
+    if(navClassList.classList[1] == 'nav--desktop') {
+        navClassList.classList.remove('nav--desktop');
+        navClassList.classList.add('nav--mobile');
+    }
+    else {
+        navClassList.classList.remove('nav--mobile');
+        navClassList.classList.add('nav--desktop');
+    }
+}
+
+btnToggleNav.addEventListener('click', (event: MouseEvent) => toggleMenu());
+
